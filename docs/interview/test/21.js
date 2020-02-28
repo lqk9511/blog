d46d3bf47bab0e8b0ceead6fee5f7703986ce029
liqiankun <870355761@qq.com>
2020-02-28 11:00:05 +0800

    feat(add question): question 21

---
+++ docs/interview/test/21.js
@@ -0,0 +1,3 @@
+console.log('TCL: instanceof String', 'Jeff' instanceof String)
+console.log('TCL: instanceof String', new String('Jeff') instanceof String)
+console.log('TCL: instanceof String', 123 instanceof Number)
