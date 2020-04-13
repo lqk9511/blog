185a61b4da4e822845294b0a5d96d82134e4c42e
liqiankun <870355761@qq.com>
2020-04-13 22:05:03 +0800

    fix(modify question): 56

--- docs/interview/README.md
+++ docs/interview/README.md
@@ -175,7 +175,7 @@
 
     如下：{1:222, 2:123, 5:888}，请把数据处理为如下结构：[222, 123, null, null, 888, null, null, null, null, null, null, null]。
 
-57. [某公司 1 到 12 月份的销售额存在一个对象里面](./question/57.md)
+57. [要求设计 LazyMan 类，实现以下功能](./question/57.md)
 
 58. [分析比较 opacity: 0、visibility: hidden、display: none 优劣和适用场景](./question/58.md)
 
--- docs/interview/test/56.js
+++ docs/interview/test/56.js
@@ -2,12 +2,10 @@ let obj = { 1: 222, 2: 123, 5: 888 }
 
 let arr = new Array(12)
 
-for (let index = 0, y = index + 1; index < arr.length; index++, y++) {
-  arr[index] = null
-  if (obj[y]) arr[index] = obj[y]
-}
+for (let index = 0, y = index + 1; index < arr.length; index++, y++)
+  arr[index] = obj[y] || null
 console.log(arr)
 
-console.log(
-  Array.from({ length: 12 }).map((_, index) => obj[index + 1] || null)
-)
+// console.log(
+//   Array.from({ length: 12 }).map((_, index) => obj[index + 1] || null)
+// )
