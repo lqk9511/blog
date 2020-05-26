31c74473ec2206d00b2c916d5f7e91ae35abb97a
liqiankun <870355761@qq.com>
2020-05-26 21:05:36 +0800

    feat(add question): test

--- docs/interview/test/test.js
+++ docs/interview/test/test.js
@@ -33,4 +33,113 @@ var reverse = function (x) {
   return res
 }
 
-console.log(reverse(123))
+// console.log(reverse(123))
+
+// const list = [1, 2, 3]
+// const square = (num) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(num * num)
+//     }, 1000)
+//   })
+// }
+
+// function test() {
+//   async function next() {
+//     if (list.length) {
+//       const res = await square(list.shift())
+//       console.log(res)
+//       next()
+//     }
+//   }
+//   next()
+// }
+// test()
+
+const testTarget = {
+  name: 'jeff',
+  reg: new RegExp(/\w\d\w/g),
+  tree: {
+    value: 'root',
+    children: [
+      {
+        value: 'A',
+        children: [
+          { value: 'A-a', children: [{ value: 'A-a-1' }, { value: 'A-a-2' }] },
+          { value: 'A-b', children: [{ value: 'A-b-1' }, { value: 'A-b-2' }] },
+        ],
+      },
+      {
+        value: 'B',
+        children: [
+          { value: 'B-a', children: [{ value: 'B-a-1' }, { value: 'B-a-2' }] },
+          { value: 'B-b', children: [{ value: 'B-b-1' }, { value: 'B-b-2' }] },
+        ],
+      },
+      {
+        value: 'C',
+        children: [
+          { value: 'C-a', children: [{ value: 'C-a-1' }, { value: 'C-a-2' }] },
+          { value: 'C-b', children: [{ value: 'C-b-1' }, { value: 'C-b-2' }] },
+        ],
+      },
+    ],
+  },
+  deepQuote: null,
+}
+
+const isType = (obj, type) =>
+  Object.prototype.toString.call(obj).slice(8, -1) === type
+
+function getEmpty(obj) {
+  if (isType(obj, 'Object')) return {}
+  if (isType(obj, 'Array')) return []
+  if (isType(obj, 'Function'))
+    return Function(
+      `"use strict"; return (function() {return ${obj.toString()}})`
+    )
+  if (isType(obj, 'RegExp')) {
+    // \w 用于匹配字母，数字或下划线字符，相当于[A-Za-z0-9_]
+    const reFlags = /\w*$/
+    const reg = new RegExp(obj.source, reFlags.exec(obj))
+    reg.lastIndex = obj.lastIndex
+    return reg
+  }
+  return obj
+}
+
+function dfsClone(target) {
+  let result = getEmpty(target),
+    stack = [],
+    recordMap = new Map()
+
+  function checkCurrent(t, r) {
+    if (t !== r) {
+      stack.push([t, r])
+      recordMap.set(t, r)
+    }
+  }
+
+  checkCurrent(target, result)
+
+  while (stack.length) {
+    let [tar, res] = stack.pop()
+    for (const key in tar) {
+      if (tar.hasOwnProperty(key)) {
+        const element = tar[key]
+
+        if (recordMap.has(element)) {
+          res[key] = recordMap.get(element)
+          continue
+        }
+
+        res[key] = getEmpty(element)
+        checkCurrent(element, res[key])
+      }
+    }
+  }
+
+  return result
+}
+
+console.log(dfsClone(testTarget))
