a3f4e3d0362add320837b584f64ce44ab5ceb3a9
liqiankun <870355761@qq.com>
2020-06-29 21:06:08 +0800

    feat(update): 100

--- docs/interview/test/100.js
+++ docs/interview/test/100.js
@@ -1,36 +1,41 @@
 // (牌底) 1,2,3,4,5,6,7,8,9,10,11,12,13 (牌顶)
 
 function shuffle(queue) {
-  console.log('shuffle -> queue', queue)
   let _res = [],
     _index = 1,
     _topValue
   while (queue.length) {
-    _topValue = queue.pop()
+    _topValue = queue.shift()
     if (_index % 2) {
       _res.push(_topValue)
     } else {
-      queue.unshift(_topValue)
+      queue.push(_topValue)
     }
     _index++
   }
   return _res
 }
 
-function recover(stack) {
+function recover(arr) {
   let _res = [],
     _index = 1
 
-  while (stack.length) {
+  while (arr.length) {
     if (_index % 2) {
-      _res.unshift(stack.shift())
+      _res.unshift(arr.pop())
     } else {
-      _res.unshift(stack.pop())
+      _res.unshift(_res.pop())
     }
+    console.log('recover -> _res', _res)
+
     _index++
   }
 
   return _res
 }
 
-console.log(shuffle(recover([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])))
+let _shuffleVal = shuffle([1, 12, 2, 8, 3, 11, 4, 9, 5, 13, 6, 10, 7])
+
+console.log(_shuffleVal)
+
+console.log(recover(_shuffleVal))
