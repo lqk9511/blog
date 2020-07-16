5a4cdec4a7ebf172726d6ef9e6bd351e8decc72d
liqiankun <870355761@qq.com>
2020-07-16 22:00:24 +0800

    feat(update): leetcode 10.js

--- docs/interview/test-leetcode/10.js
+++ docs/interview/test-leetcode/10.js
@@ -4,9 +4,33 @@
  * @return {boolean}
  */
 var isMatch = function (s, p) {
-  let _length = s.length,
-    dp = Array.from(new Array(length), () => new Array(length).fill(0)),
-    _res = false
+  let m = s.length,
+    n = p.length,
+    dp = Array.from(new Array(m + 1), () => new Array(n + 1).fill(false))
 
-  for (let i = 0; i < _length; i++) {}
+  dp[0][0] = true
+
+  for (let i = 1; i < m + 1; i++) {
+    for (let j = 1; j < n + 1; j++) {
+      if (s[i - 1] === p[j - 1] || p[j - 1] === '.') {
+        dp[i][j] = dp[i - 1][j - 1]
+      } else if (p[j - 1] === '*') {
+        if (s[i - 1] === p[j - 2] || p[j - 2] === '.') {
+          dp[i][j] = dp[i][j - 2] || dp[i - 1][j - 2] || dp[i - 1][j]
+        } else {
+          dp[i][j] = dp[i][j - 2]
+        }
+      }
+
+      console.log(dp.slice(0, 1).toString())
+      console.log(dp.slice(1, 2).toString())
+      console.log(dp.slice(2, 3).toString())
+      console.log('-------------------------------')
+    }
+  }
+
+  console.log(dp)
+  return dp[m][n]
 }
+
+console.log(isMatch('aa', '.*b*'))
