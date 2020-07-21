2bc4a033b76fedab12fddb00cf322e44a5864b06
liqiankun <870355761@qq.com>
2020-07-21 22:02:21 +0800

    feat(update): leetcode 10

--- docs/interview/test-leetcode/10.js
+++ docs/interview/test-leetcode/10.js
@@ -1,3 +1,36 @@
+// /**
+//  * @param {string} s
+//  * @param {string} p
+//  * @return {boolean}
+//  */
+// var isMatch = function (s, p) {
+//   let m = s.length,
+//     n = p.length,
+//     dp = Array.from(new Array(m + 1), () => new Array(n + 1).fill(false))
+
+//   dp[0][0] = true
+
+//   for (let j = 1; j < n + 1; j++) {
+//     if (p[j - 1] == '*') dp[0][j] = dp[0][j - 2]
+//   }
+
+//   for (let i = 1; i < m + 1; i++) {
+//     for (let j = 1; j < n + 1; j++) {
+//       if (s[i - 1] === p[j - 1] || p[j - 1] === '.') {
+//         dp[i][j] = dp[i - 1][j - 1]
+//       } else if (p[j - 1] === '*') {
+//         if (s[i - 1] === p[j - 2] || p[j - 2] === '.') {
+//           dp[i][j] = dp[i][j - 2] || dp[i - 1][j - 2] || dp[i - 1][j]
+//         } else {
+//           dp[i][j] = dp[i][j - 2]
+//         }
+//       }
+//     }
+//   }
+
+//   return dp[m][n]
+// }
+
 /**
  * @param {string} s
  * @param {string} p
@@ -8,35 +41,22 @@ var isMatch = function (s, p) {
     n = p.length,
     dp = Array.from(new Array(m + 1), () => new Array(n + 1).fill(false))
 
+  const matches = (i, j) =>
+    i === 0 ? false : p[j - 1] === '.' || s[i - 1] === p[j - 1]
+
   dp[0][0] = true
 
-  for (let j = 1; j < n + 1; j++) {
-    if (p[j - 1] == '*') dp[0][j] = dp[0][j - 2]
-  }
-
-  for (let i = 1; i < m + 1; i++) {
-    for (let j = 1; j < n + 1; j++) {
-      if (s[i - 1] === p[j - 1] || p[j - 1] === '.') {
+  for (let i = 0; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (p[j - 1] === '*') {
+        dp[i][j] = dp[i][j - 2]
+        if (matches(i, j - 1)) dp[i][j] = dp[i][j] || dp[i - 1][j]
+      } else if (matches(i, j)) {
         dp[i][j] = dp[i - 1][j - 1]
-      } else if (p[j - 1] === '*') {
-        if (s[i - 1] === p[j - 2] || p[j - 2] === '.') {
-          dp[i][j] = dp[i][j - 2] || dp[i - 1][j - 2] || dp[i - 1][j]
-        } else {
-          dp[i][j] = dp[i][j - 2]
-        }
       }
-
-      console.log(dp.slice(0, 1).toString())
-      console.log(dp.slice(1, 2).toString())
-      console.log(dp.slice(2, 3).toString())
-      console.log('-------------------------------')
     }
-
-    console.log('-------------next-------------')
   }
 
-  console.log(dp)
   return dp[m][n]
 }
-
-console.log(isMatch('aab', '*'))
+console.log(isMatch('ab', '.*'))
