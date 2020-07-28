712613235214c682e7b6d263b1ce83c724728e9f
liqiankun <870355761@qq.com>
2020-07-28 21:58:27 +0800

    feat(update): leetcode 11

--- docs/interview/test-leetcode/11.js
+++ docs/interview/test-leetcode/11.js
@@ -1,16 +1,38 @@
+// /**
+//  * @param {number[]} height
+//  * @return {number}
+//  */
+// var maxArea = function (height) {
+//   let length = height.length,
+//     res = 0
+
+//   for (let i = 0; i < length; i++) {
+//     for (let j = i + 1; j < length; j++) {
+//       res = Math.max(res, (j - i) * Math.min(height[i], height[j]))
+//     }
+//   }
+//   return res
+// }
+
 /**
  * @param {number[]} height
  * @return {number}
  */
 var maxArea = function (height) {
   let length = height.length,
+    index = 0,
+    lastIndex = length - 1,
     res = 0
 
-  for (let i = 0; i < length; i++) {
-    for (let j = i + 1; j < length; j++) {
-      res = Math.max(res, (j - i) * Math.min(height[i], height[j]))
-    }
+  while (index != lastIndex) {
+    let diffIndex = lastIndex - index,
+      indexValue = height[index],
+      lastIndexValue = height[lastIndex]
+    res = Math.max(res, Math.min(indexValue, lastIndexValue) * diffIndex)
+    if (indexValue > lastIndexValue) lastIndex--
+    else index++
   }
+
   return res
 }
 
