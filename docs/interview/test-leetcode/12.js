6e74f420b1b9c61a2002dc333e266f39d2b0f027
liqiankun <870355761@qq.com>
2020-08-24 15:49:10 +0800

    feat(leetcode): 12 贪心

--- docs/interview/test-leetcode/12.js
+++ docs/interview/test-leetcode/12.js
@@ -3,58 +3,35 @@
  * @return {string}
  */
 var intToRoman = function (num) {
-  const _romanMap = new Map([
-    [1000, 'M'],
-    [500, 'D'],
-    [100, 'C'],
-    [50, 'L'],
-    [10, 'X'],
-    [5, 'V'],
-    [1, 'I'],
-  ])
+  const _keys = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
+    _values = [
+      'M',
+      'CM',
+      'D',
+      'CD',
+      'C',
+      'XC',
+      'L',
+      'XL',
+      'X',
+      'IX',
+      'V',
+      'IV',
+      'I',
+    ]
+
   let res = ''
 
   if (num > 3999) num = 3999
   if (num < 1) num = 1
 
-  for (const [key, value] of _romanMap) {
-    const multiple = num / key
-    num %= key
-    if (multiple === 9) {
-      if (key === 1) {
-        res += `${value}X`
-      } else if (key === 10) {
-        res += `${value}C`
-      } else if (key === 100) {
-        res += `${value}M`
-      }
-    } else if (multiple > 1) {
-      if (multiple === 4) {
-        if (key === 1) {
-          res += `${value}V`
-        } else if (key === 10) {
-          res += `${value}L`
-        } else if (key === 100) {
-          res += `${value}D`
-        }
-      } else {
-        res += stringMul(value, Math.floor(multiple))
-      }
-    } else if (multiple === 1) {
-      res += value
-      return res
+  for (let i = 0; i < _keys.length; i++) {
+    while (num >= _keys[i]) {
+      num -= _keys[i]
+      res += _values[i]
     }
   }
-
   return res
 }
 
-function stringMul(s, mul) {
-  res = ''
-  for (let i = 0; i < mul; i++) {
-    res += s
-  }
-  return res
-}
-
-console.log(intToRoman(1994))
+console.log(intToRoman(58))
