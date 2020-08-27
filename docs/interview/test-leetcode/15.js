35e09d913845a1c828b4702a516b713b31152e8d
liqiankun <870355761@qq.com>
2020-08-27 19:56:23 +0800

    feat(leetcode): 15

--- docs/interview/test-leetcode/15.js
+++ docs/interview/test-leetcode/15.js
@@ -26,8 +26,38 @@ var threeSum = function (nums) {
   return [...res.values()]
 }
 
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum2 = function (nums) {
+  let length = nums.length,
+    res = []
+  nums = nums.sort((a, b) => a - b)
+
+  for (let first = 0; first < length; first++) {
+    if (first > 0 && nums[first] === nums[first + 1]) continue
+
+    let third = length - 1,
+      target = -nums[first]
+
+    for (let second = first + 1; second < length; second++) {
+      if (second > first + 1 && nums[second] == nums[second - 1]) continue
+
+      while (second < third && nums[second] + nums[third] > target) --third
+
+      if (second === third) break
+
+      if (nums[second] + nums[third] == target) {
+        res.push([nums[first], nums[second], nums[third]])
+      }
+    }
+  }
+  return [...res.values()]
+}
+
 console.log(
-  threeSum([
+  threeSum2([
     82597,
     -9243,
     62390,
