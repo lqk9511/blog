f9729a0e58a7cdac95c4bc012f672e482b71133a
liqiankun <870355761@qq.com>
2020-09-08 19:10:30 +0800

    feat(leetcode): update 17

--- docs/interview/test-leetcode/17.js
+++ docs/interview/test-leetcode/17.js
@@ -2,4 +2,36 @@
  * @param {string} digits
  * @return {string[]}
  */
-var letterCombinations = function (digits) {}
+var letterCombinations = function (digits) {
+  const _map = {
+    2: 'abc',
+    3: 'def',
+    4: 'ghi',
+    5: 'jkl',
+    6: 'mno',
+    7: 'pqrs',
+    8: 'tuv',
+    9: 'wxyz',
+  }
+  let res = [],
+    digitsValue = []
+
+  for (let i = 0; i < digits.length; i++) {
+    digitsValue.push(_map[digits[i]])
+  }
+  backtrack('', 0)
+
+  function backtrack(letter, index) {
+    if (index === digitsValue.length) {
+      res.push(letter)
+      return
+    }
+    for (const l of digitsValue[index]) {
+      backtrack(letter + l, index + 1)
+    }
+  }
+
+  return res
+}
+
+console.log(letterCombinations('23'))
